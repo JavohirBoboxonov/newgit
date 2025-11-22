8b09484955e06e1b3cc0ce7aea045fa90630af18
JavohirBoboxonov <javohirboboxonov08@gmail.com>
2025-11-22 15:37:35 +0500

    update commit

--- meta/shop.py
+++ meta/shop.py
@@ -1,135 +1,145 @@
-import json
-import os
-class Contact:
-    def __init__(self, name, phone_number, email):
+class Product:
+    def __init__(self, name, price, year, product_type):
         self.name = name
-        self.phone_number = phone_number
-        self.email = email
+        self.price = price
+        self.year = year
+        self.product_type = product_type
 
-    def get_info_contact(self):
+    def get_info(self):
         return f"""
-    Name:{self.name}
-    Phone Number:{self.phone_number}
-    Email:{self.email}
+        Name:{self.name}
+        Price: {self.price}
+        Year: {self.year}
+        Product Type: {self.product_type}
     """
-    def to_dict(self):
-        return {
-            "name":self.name,
-            "phone_number":self.phone_number,
-            "email":self.email
-        }
-class Smsmanager:
-    def __init__(self, name, phone_number):
-        self.name = name
-        self.phone_number = phone_number
 
-    def take_sms(self,receiver_phone):
-        try:
-            with open("contacts.json", "r") as contacts:
-                data = json.load(contacts)
-                if receiver_phone in data:
-                    print(f"sms {receiver_phone}ga yuborildi")
-        except (FileNotFoundError, json.JSONDecoder) as r:
-            print(f"Xato {r}")
-class Phone:
-    def __init__(self, name, filename):
-        self.name = name
-        self.filename = filename
-        self.contacts = []
+class ElectronicProduct(Product):
+    def __init__(self, name, price, year, product_type, battery):
+        super().__init__(name, price, year, product_type)
+        self.battery = battery
+        self.electronic_products = []
 
-    def load_json(self):
-        if os.path.exists(self.filename):
-            with open(os.path.join(self.filename), "r") as f:
-                data = json.load(f)
-                for item in data:
-                    main = Contact(item["name"], item["phone_number"], item["email"])
-                    self.contacts.append(main)
+    def add_electronic_products(self, new_product):
+        if new_product not in self.electronic_products:
+            self.electronic_products.append(new_product)
 
-    def save_json(self):
-        data = [key.to_dict() for key in self.contacts]
-        with open(self.filename, "w") as name:
-            json.dump(data, name, indent=4)
-
-    def add_contact(self, new_contact):
-        if new_contact not in self.contacts:
-            self.contacts.append(new_contact)
-            self.save_json()
-
-    def update_contact(self, old_name, new_name = None, new_phone_number = None, new_email = None,):
-        for item in self.contacts:
-            if item.name == old_name:
-                if new_name:
-                    item.name = new_name
-                if new_phone_number:
-                    item.phone_number = new_phone_number
-                if new_email:
-                    item.email = new_email
-                self.save_json()
-                return True
-            return False
-    def remove_contact(self, deleted_contact):
-        b = [x for x in self.contacts if x == deleted_contact]
-        if len(b) > 0:
-            self.contacts.remove(deleted_contact)
-            self.save_json()
+    def remove_electronic_products(self, removed_product):
+        if removed_product in self.electronic_products:
+            self.electronic_products.remove(removed_product)
         else:
-            print("xato")
-    def view_contacts(self):
-        for item in self.contacts:
-            print(item.to_dict())
-s1 = Phone("Iphone", "contacts.json")
+            print("Afsuski Bu Mahsulot Topilmadi")
+    def info_all_products(self):
+        for i in self.electronic_products:
+            print(i.get_info())
 
-def menu():
-    print("\nPhone Book Options:")
-    print("1. View Contacts")
-    print("2. Add Contact")
-    print("3. Update Contact")
-    print("4. Remove Contact")
-    print("5. Exit")
+    def get_info(self):
+        return f"""
+Name:{self.name}
+Price:{self.price}
+Year:{self.year}
+Product:{self.product_type}
+Battery:{self.battery}
+"""
 
-def main():
-    while True:
-        menu()
-        choice = input("Choose an option (1-5): ").strip()
+class Furniture(Product):
+    def __init__(self, name, price, year, product_type, quality):
+        super().__init__(name, price, year, product_type)
+        self.quality = quality
+        self.furniture_products = []
 
-        if choice == '1':
-            # View all contacts
-            s1.view_contacts()
+    def add_furniture_products(self, new_products):
+        if new_products not in self.furniture_products:
+            self.furniture_products.append(new_products)
 
-        elif choice == '2':
-            name = input("Enter name: ").strip()
-            phone_number = input("Enter phone number: ").strip()
-            email = input("Enter email: ").strip()
-            new_contact = Contact(name, phone_number, email)
-            s1.add_contact(new_contact)
-            print(f"Contact {name} added.")
-
-        elif choice == '3':
-            old_name = input("Enter the name of the contact you want to update: ").strip()
-            new_name = input("Enter new name (or press Enter to skip): ").strip() or None
-            new_phone_number = input("Enter new phone number (or press Enter to skip): ").strip() or None
-            new_email = input("Enter new email (or press Enter to skip): ").strip() or None
-            if s1.update_contact(old_name, new_name, new_phone_number, new_email):
-                print(f"Contact {old_name} updated.")
-            else:
-                print(f"Contact {old_name} not found.")
-
-        elif choice == '4':
-            name_to_remove = input("Enter the name of the contact you want to remove: ").strip()
-            contact_to_remove = None
-            for contact in s1.contacts:
-                if contact.name == name_to_remove:
-                    contact_to_remove = contact
-                    break
-            if contact_to_remove:
-                s1.remove_contact(contact_to_remove)
-                print(f"Contact {name_to_remove} removed.")
-            else:
-                print(f"Contact {name_to_remove} not found.")
-
-        elif choice == '5':
-            print("Exiting phone book...")
-            break
+    def remove_products(self, removed_products):
+        if removed_products in self.furniture_products:
+            self.furniture_products.remove(removed_products)
         else:
-            print("Invalid option. Please choose between 1-5.")
-main()
+            print("Bunday Mahsulot Topilmadi")
+
+    def get_info(self):
+        return f"""
+Name:{self.name}
+Price:{self.price}
+Product Type:{self.product_type}
+Year:{self.year}
+Sifati:{self.quality}
+"""
+
+class Os(Product):
+    def __init__(self, name, price, year, product_type, version):
+        super().__init__(name, price, year, product_type)
+        self.version = version
+        self.operation_systems = []
+
+    def add_operating_system(self, new_system):
+        if new_system not in self.operation_systems:
+            self.operation_systems.append(new_system)
+
+    def remove_system(self, removed_system):
+        if removed_system in self.operation_systems:
+            self.operation_systems.remove(removed_system)
+        else:
+            print("Bunday Os Topilmadi")
+
+    def get_info(self):
+        return f"""
+Name: {self.name}
+Price: {self.price}
+Product Type: {self.product_type}
+Year: {self.year}
+System:{self.version}
+"""
+
+class Shop:
+    def __init__(self, name):
+        self.name = name
+        self.products = []
+
+    def add_product(self, new_product):
+        if new_product not in self.products:
+            self.products.append(new_product)
+
+    def remove_products(self, removed_products):
+        if removed_products in self.products:
+            self.products.remove(removed_products)
+
+    def update_product_info(self, product_name, **kwargs):
+        for product in self.products:
+            if product.name == product_name:
+                for key, value in kwargs.items():
+                    if hasattr(product, key):
+                        setattr(product, key, value)
+                    else:
+                        print(f"{key} - {product_name} mahsulotida bunday atribut yo'q.")
+                print(f"{product_name} mahsuloti muvaffaqiyatli yangilandi.")
+                return
+        print(f"{product_name} nomli mahsulot topilmadi.")
+    def get_systems(self):
+        for i in self.products:
+            if i.product_type == "os":
+                print(f"Os Turidagi Mahsulotlar {i.get_info()}")
+
+    def get_furniture(self):
+        for i in self.products:
+            if i.product_type == "furniture":
+                print(f"Mebel Turidagi Mahsulotlar {i.get_info()}")
+    def get_electronic(self):
+        for i in self.products:
+            if i.product_type == "electron":
+                print(f"ELectron Qurilmalar {i.get_info()}")
+
+    def get_info(self):
+        for i in self.products:
+            print(i.get_info())
+d1 = Shop("Pepsi Market")
+
+s1 = Os("Windows", "10$ for month", 2021, "os", "11")
+s2 = Furniture("Shkaf", 400000, 2025, "furniture", "A`lo")
+s3 = ElectronicProduct("Hp noutbook", 5000000, 2023, "electron", "4000 mA")
+d1.add_product(s1)
+d1.add_product(s2)
+d1.add_product(s3)
+print(d1.get_systems())
+print(d1.get_electronic())
+print(d1.get_furniture())
